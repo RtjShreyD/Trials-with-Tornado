65079b1e8a0eb01b1625010479225e1002624007
RtjShreyD <shreyanshece1041@gmail.com>
2020-03-30 00:45:46 +0530

    Resource req handler

--- index.py
+++ index.py
@@ -5,23 +5,29 @@ class QueryStringHandler(tornado.web.RequestHandler):
     def get(self):
         n=int(self.get_argument("n"))
         r= "odd" if n%2 else "even"
-        self.write("The number is" + r)
+        self.write("The number is " + r)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class ResourceRequestHandler(tornado.web.RequestHandler):
+    def get(self, id):
+        self.write("Tweet Id is " + id)
+
 
 class StaticRequesthandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/blog", StaticRequesthandler),
-        (r"/isEven", QueryStringHandler)
+        (r"/isEven", QueryStringHandler),
+        (r"/tweet/([0-9]+)", ResourceRequestHandler)
     ])
 
 if __name__ == "__main__":
