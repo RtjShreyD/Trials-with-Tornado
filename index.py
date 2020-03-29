27f6cb23c4248ce49bd152003979b84d3434cc27
RtjShreyD <shreyanshece1041@gmail.com>
2020-03-30 00:27:53 +0530

    Query, static file tested

--- index.py
+++ index.py
@@ -1,13 +1,27 @@
 import tornado.ioloop
 import tornado.web
 
+class QueryStringHandler(tornado.web.RequestHandler):
+    def get(self):
+        n=int(self.get_argument("n"))
+        r= "odd" if n%2 else "even"
+        self.write("The number is" + r)
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
+class StaticRequesthandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/blog", StaticRequesthandler),
+        (r"/isEven", QueryStringHandler)
     ])
 
 if __name__ == "__main__":
